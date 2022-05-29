6a75c7897b2e5914684ee0c739c43070cee54387
Vishal Adithya.A <98593482+Adithegeek@users.noreply.github.com>
2022-05-29 18:10:53 +0530

    Add files via upload

---
+++ web_scraping.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+class scrap():
+    
+    def file_scrap(self):
+        file_name = input("enter the file name which you need to access: ")
+        with open (file_name, "r") as f:
+            doc = BeautifulSoup(f, "html.parser")
+        print(doc.prettify())
+    
+    def web_scrap():
+        link = input("enter the website link which you needed to access: ")
+        result = requests.get(link)
+        doc = BeautifulSoup(result.text, "html.parser")
+        print(doc.prettify())
+
+x = scrap
+x.web_scrap()
+
